3b9333163a3603cc077493229c5d5c739768b3a5
gergovie727 <yohann.letellier.pro@gmail.com>
2024-02-12 19:53:03 +0100

    12.02.2024.19.53
    Ajout de la fonction de présentation de l'aide ainsi que de sa présentation en cas d'absence d'option.

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ import chiffrement_cesar
 import sys
 
 def show_help():
-    pass
+    print("Usage: cesarpy [OPTIONS]\n")
+
+    print("Description:\n\tScript pour chiffrer un message en clair et déchiffrer ou décrypter un message chiffré par le code de César.\n")
+
+    print("Options:")
+    print("\t-h, --help\t\t Affiche l'aide.")
+    print("\t-v, --version\t\t Affiche la version du script.")
+    print("\t-f, --file\t\t Permet de spécifier le chemin du fichier dans lequel est contenu le texte.")
+    print("\t-t, --texte\t\t Permet de spécifier le texte directement en ligne de commande.")
+    print("\t-c, --chiffrement\t\t Permet de chiffrer le texte.")
+    print("\t-d, --dechiffrement\t\t Permet de déchiffrer le texte.")
+    print("\t-D, --decryptage\t\t Permet de décrypter le texte.")
+
+if len(sys.argv) == 1:
+    show_help()
